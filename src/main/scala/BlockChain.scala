6dfe301b47f8a30d159027366e33de35272ce76c
James Duley <jagduley@gmail.com>
2021-03-21 02:22:32 +0000

    Remove unneeded self hash from Block

--- src/main/scala/BlockChain.scala
+++ src/main/scala/BlockChain.scala
@@ -2,21 +2,17 @@ type Data = List[Byte] // TODO: should this be properly typed?
 
 type HashT = List[Byte] // TODO: sort out name conflict with Hash
 
-case class Block(hash: HashT, prevHash: HashT, data: Data)
+case class Block(prevHash: HashT, data: Data):
+    def hash: HashT =
+        Hash.sha256(prevHash ++ data)
 
 // TODO: should BlockChain be its own class? probably yes
 object BlockChain:
     val genesisHash: HashT = List.empty
 
-    def calculateHash(prevHash: HashT, data: Data): HashT =
-        Hash.sha256(prevHash ++ data)
-
-    def verify(chain: List[Block]): Boolean =
-        val isInvalidBlock = (block: Block, prevHash: HashT) =>
-            prevHash != block.prevHash || calculateHash(block.prevHash, block.data) != block.hash
-        val prevHashes = genesisHash +: chain.view.map(_.hash)
-        !chain.view.zip(prevHashes).exists(isInvalidBlock.tupled)
+    def isValid(chain: List[Block]): Boolean =
+        !chain.view.sliding(2).exists(pair => pair.head.hash != pair.last.prevHash)
 
     def addBlock(chain: List[Block], data: Data): List[Block] =
         val prevHash = if chain.isEmpty then genesisHash else chain.last.hash
-        chain :+ Block(calculateHash(prevHash, data), prevHash, data)
+        chain :+ Block(prevHash, data)
