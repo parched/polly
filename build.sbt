b7a0b434e68cf7924f1d1141a5ad82d8c692951e
James Duley <jagduley@gmail.com>
2021-03-21 03:10:20 +0000

    Add REST app

--- build.sbt
+++ build.sbt
@@ -1,12 +1,18 @@
+val AkkaVersion = "2.6.8"
+val AkkaHttpVersion = "10.2.4"
+
 lazy val root = project
   .in(file("."))
   .settings(
     name := "polly",
     version := "0.1.0",
-
     scalaVersion := "3.0.0-RC1",
-
     libraryDependencies += "com.novocode" % "junit-interface" % "0.11" % "test",
-
-    useScala3doc := true,
+    libraryDependencies ++= Seq(
+      "com.typesafe.akka" %% "akka-actor-typed" % AkkaVersion,
+      "com.typesafe.akka" %% "akka-stream" % AkkaVersion,
+      "com.typesafe.akka" %% "akka-http" % AkkaHttpVersion,
+      "com.typesafe.akka" %% "akka-http-spray-json" % AkkaHttpVersion
+    ).map(_.withDottyCompat(scalaVersion.value)),
+    useScala3doc := true
   )
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,58 @@
+import akka.actor.typed.ActorSystem
+import akka.actor.typed.scaladsl.Behaviors
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.*
+import akka.http.scaladsl.server.Directives.*
+import akka.util.ByteString
+import scala.io.StdIn
+
+import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport
+import spray.json.*
+
+trait JsonSupport extends SprayJsonSupport with DefaultJsonProtocol:
+    import java.util.Base64
+
+    val base64encoder = Base64.getEncoder()
+    val base64decoder = Base64.getDecoder()
+    def toBase64(data: List[Byte]): String =
+        base64encoder.encodeToString(data.toArray)
+    def fromBase64(data: String): List[Byte] =
+        base64decoder.decode(data).toList
+
+    implicit object BlockJsonFormat extends RootJsonFormat[Block]:
+        def write(b: Block) =
+            JsObject("prev_hash" -> JsString(toBase64(b.prevHash)), "data" -> JsString(toBase64(b.data)))
+
+        def read(value: JsValue) = value.asJsObject.getFields("prev_hash", "data") match
+            case Seq(JsString(prevHash), JsString(data)) =>
+             Block(fromBase64(prevHash), fromBase64(data))
+            case _ => throw DeserializationException("Block expected")
+
+
+object Main extends JsonSupport:
+
+    def main(args: Array[String]): Unit =
+
+        implicit val system = ActorSystem(Behaviors.empty, "my-system")
+        // needed for the future flatMap/onComplete in the end
+        implicit val executionContext = system.executionContext
+
+        var blockChain = List.empty[Block]
+
+        val route = concat(
+            (path("blocks") & get) {
+                complete(blockChain.toJson) // TODO: what import is needed to avoid toJson?
+            },
+            (path("data") & post & entity(as[ByteString])) { data =>
+                blockChain = BlockChain.addBlock(blockChain, data.toList)
+                complete("block created")
+            }
+        )
+
+        val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+
+        println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
+        StdIn.readLine() // let it run until user presses return
+        bindingFuture
+            .flatMap(_.unbind()) // trigger unbinding from the port
+            .onComplete(_ => system.terminate()) // and shutdown when done
