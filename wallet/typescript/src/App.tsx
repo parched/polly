67160d9b062c7d9ed9e04f41ba4a0945823c980f
James Duley <jagduley@gmail.com>
2021-05-09 03:50:41 +0000

    Add server input

--- wallet/typescript/src/App.tsx
+++ wallet/typescript/src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ServerForm from './ServerForm'
 
 function App() {
   return (
@@ -8,16 +9,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          <ServerForm defaultUrl="http://localhost:8080" />
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
